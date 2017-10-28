4dba9b9845740dbe298a283e484f22dd22ebace0
Gino Biervliet <gino@documentready.nl>
2017-10-28 16:53:19 +0200

    Fixed construction bug

--- src/BuilderCreep.js
+++ src/BuilderCreep.js
@@ -3,14 +3,18 @@ var CreepRoles = require('CreepRoles');
 module.exports = {
 
     run(creep) {
-        let constuctionSites = Object.keys(Game.constructionSites);
-
-        if(constuctionSites.length != 0) {
-            let site = constuctionSites.get(0);
-            if(!creep.build(site)) {
-                creep.moveTo(constuctionSites.get(0));
+        let constructionSites = Object.keys(Game.constructionSites);
+        const total = _.sum(creep.carry);
+        if (constructionSites.length != 0) {
+            if (total == 0) {
+                creep.memory.role = CreepRoles.HARVESTER();
             }
-        } else{
+            let site = Game.constructionSites[constructionSites[0]];
+            console.log(site);
+            if (creep.build(site) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(site);
+            }
+        } else {
             creep.memory.role = CreepRoles.HARVESTER();
         }
 
